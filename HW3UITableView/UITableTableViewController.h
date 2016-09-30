e94db4510683438e0a60401ebd279f4a5ccedd6d
Rustamnurg <Rustik6030@mail.ru>
2016-09-30 21:08:51 +0300

    HW3

---
+++ HW3UITableView/UITableTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  UITableTableViewController.h
+//  HW3UITableView
+//
+//  Created by Rustam N on 28.09.16.
+//  Copyright © 2016 com.ildar.itis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableTableViewController : UITableViewController
+
+@end
