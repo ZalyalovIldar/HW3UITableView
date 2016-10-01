e4e59ba425be2e25490d5d5ae1ff04c2a77e0e74
gulia-is <iskandarova.g.f@mail.ru>
2016-10-01 23:58:00 +0300

    ThirdHomeWork

---
+++ HW3UITableView/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  HW3UITableView
+//
+//  Created by Gulia_Is on 28.09.16.
+//  Copyright © 2016 com.ildar.itis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
